5b9f7d39c92ee4720f77e420d66c75d99c8db431
my name <blueshaperd3@gmail.com>
2024-03-16 07:09:12 +0500

    loops for

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-for(a = 0; a < 5;  a++){
-    alert("Hello World")
+//for(a = 0; a < 5;  a++){
+  //alert("Hello World")
+//}
+
+//var names = ["Rehan", "Ali", "Kamran"];
+
+//for(var i = 0; i < names.length; i++){
+  //  console.log(names[i])
+//}
+
+var cities = ["Lahore", "Islamabad", "Karachi", "Rawalpindi", "Quetta"];
+
+for(var i = 0; i < cities.length; i++){
+    if(cities[i] === "Karachi"){
+        alert(cities[i] + " is the city of lights")
+    }
 }
